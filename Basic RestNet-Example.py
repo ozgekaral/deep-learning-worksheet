a7e79fdf762e1785c4b0083219ab6e04a7fca50c
Özge Karalı <karalozge@gmail.com>
2023-03-27 13:31:37 +0300

    Add files via upload

---
+++ Basic RestNet-Example.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 11:15:52 2023
+
+@author: user202
+"""
+
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
+
+img_path = 'images/dog.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+
+pred = model.predict(x)
+
+decode_predictions(pred, top=1)
+
